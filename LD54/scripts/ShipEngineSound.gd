baaf9d2e66b80dd145cff1155fa1412deba43d16
Björn Bosse <b.bosse1991@gmail.com>
2023-10-05 22:03:15 +0200

    Added sfx for the ship engine and generator repair

---
+++ LD54/scripts/ShipEngineSound.gd
@@ -0,0 +1,13 @@
+class_name ShipEngineSound extends Node
+
+@export var sound: AudioStream
+
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+func _process(delta):
+	if Input.is_action_pressed("up"):
+		if not audio_stream_player.playing:
+			audio_stream_player.play()
+	else:
+		if audio_stream_player.playing:
+			audio_stream_player.stop()
